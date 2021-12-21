79b39790d1d824af4882a6269ca06ebfadac8e1a
nauxnam <hsnnbq@gmail.com>
2021-12-21 21:22:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client({partials:["MESSAGE", "CHANNEL", "REACTION"]});
 
+// Token Dosyası
 const {
     token
 } = require("./token.json");
